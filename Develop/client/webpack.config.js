c4ef36ffc9bd8202c3eaf193c5689a8a4ccaac42
kmart134 <ksmgps@hotmail.com>
2022-08-19 22:36:33 -0400

    webpack.config updated

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -20,28 +20,30 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        //template
-        //title
+        template: './index.html',
+        title: 'Text Editor'
       }),
       new InjectManifest({
-        //swSrc
-        //swDest
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
-        //fingerprints
-        //inject
-        //name
-        //short_name
-        //description
-        //background_color
-        //theme_color
-        //start_url
-        //publicPath
-          icons:[
-            //src
-            //sizes
-            //destination
-          ]
+        fingerprints: false,
+        inject: true,
+        name: 'JATE:Text Editor',
+        short_name: 'JATE',
+        description: 'Edit Text on the GO!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        //publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       })
       
     ],
